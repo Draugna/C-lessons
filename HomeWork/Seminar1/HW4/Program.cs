5ed03f40bfeb73e25cf69eb3003bd975b0e7553b
ZaytsevM <mivlz@mail.ru>
2022-08-09 17:42:28 +0300

    Проверка всех четных чисел до числа N

---
+++ HomeWork/Seminar1/HW4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+//Запись числа до которого будем писать все четные/нечестные числа
+Console.Write("Вставьте число = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int number = 0;
+
+// Вывод записи для всех четных чисел
+Console.Write("Четные числа до числа " + numberA + " равны: ");
+
+//Логика по выписыванию всех четных/нечотных чисел до заданного числа
+while(count <= numberA)
+    {
+        if ((count % 2) == 0)
+        {
+            number = count;
+            Console.Write(number + ", ");
+            count++;
+            
+        }
+        else
+        {  
+        count++;    
+        }
+    }
+ 
