1622221a3d0c2922a488ab73510bef0258d17b51
ZaytsevM <mivlz@mail.ru>
2022-08-09 21:44:09 +0300

    Кратно ли число 7 и 23

---
+++ Seminar/exe007/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 & number % 23 ==0)
+    {
+        Console.WriteLine("Кратно 7 и 23");
+    }
+    else {
+        Console.WriteLine("Не кратно 7 и 23");
+    }
