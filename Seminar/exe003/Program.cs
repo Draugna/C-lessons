6e4edc0d032fb7c16d248b875873cd583944d25c
ZaytsevM <mivlz@mail.ru>
2022-08-09 21:11:04 +0300

    Проверка чисел являются ли они квадратами друг друга

---
+++ Seminar/exe003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b)
+    {
+        Console.Write("Второе число является квадратом первого числа");
+    }
+else if (b * b == a)
+    {
+        Console.Write("Первое число является квадратом второго числа");
+    }
+    else{
+        Console.Write("Числа не являются квадратами друг друга");
+    }
