0beddef338876f01b3b3bab7e2f012bfe4f99e2b
ZaytsevM <mivlz@mail.ru>
2022-08-09 22:11:06 +0300

    Сумма или Разность или Равность рандомных чисел

---
+++ Dop_Zadaniya/Zadanie002/Program.cs
@@ -0,0 +1,21 @@
+﻿//Генерируются два случайных целых числа a и b в интервале [10 – 30].
+//Если a > b, выводится их разность, eсли a < b, выводится сумма,
+//eсли a == b, выводится сообщение “числа равны”.
+Console.Clear();
+int a = new Random().Next(10,31);
+Console.WriteLine(a);
+int b = new Random().Next(10,31);
+Console.WriteLine(b);
+
+if (a>b)
+    {
+        Console.WriteLine($"Разность чисел равна {a-b}");
+    }
+    else if (a<b)
+    {
+        Console.WriteLine($"Сумма чисел равна {a+b}");
+    }
+    else 
+    {
+        Console.WriteLine("Числа равны");
+    }
