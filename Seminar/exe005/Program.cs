3886188c2d65158803acedc39d500aecb11aa362
ZaytsevM <mivlz@mail.ru>
2022-08-09 22:13:10 +0300

    Кратность числа 7 и 23

--- Seminar/exe005/Program.cs
+++ Seminar/exe005/Program.cs
@@ -1,21 +1,10 @@
-﻿//Генерируются два случайных целых числа a и b в интервале [10 – 30].
-//Если a > b, выводится их разность, eсли a < b, выводится сумма,
-//eсли a == b, выводится сообщение “числа равны”.
-Console.Clear();
-int a = new Random().Next(10,31);
-Console.WriteLine(a);
-int b = new Random().Next(10,31);
-Console.WriteLine(b);
-
-if (a>b)
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 & number % 23 ==0)
     {
-        Console.WriteLine($"Разность чисел равна {a-b}");
+        Console.WriteLine("Кратно 7 и 23");
     }
-    else if (a<b)
-    {
-        Console.WriteLine($"Сумма чисел равна {a+b}");
-    }
-    else 
-    {
-        Console.WriteLine("Числа равны");
+    else {
+        Console.WriteLine("Не кратно 7 и 23");
     }
