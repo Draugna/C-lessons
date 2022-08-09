1f1a6e984fa51e9c2d9b5a96f5b2098f66020a5c
ZaytsevM <mivlz@mail.ru>
2022-08-09 17:12:19 +0300

    Проверка на четное/нечетное число

---
+++ HomeWork/Seminar1/HW3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+//Запись числа для проверки на Четное/Нечетное
+Console.Write("Вставьте число = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+string proverka;
+//Логика по Проверке четности/нечотности
+ 
+if( (numberA % 2) == 0)
+    {proverka = " Четное";}
+    else
+    {  
+        proverka = " Нечетное";    
+     };
+
+
+    Console.Write("Число " + numberA + proverka);
