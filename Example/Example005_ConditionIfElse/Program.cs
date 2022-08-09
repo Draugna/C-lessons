ec1ce4991b7ebcf79708fe0888a9b0e0617ae721
ZaytsevM <mivlz@mail.ru>
2022-08-09 15:33:28 +0300

    Example005 ToLower

--- Example/Example005_ConditionIfElse/Program.cs
+++ Example/Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
     {
         Console.WriteLine("Ура, это же МАША");
     }
 else
-{
-    Console.Write ("Привет, ");
-    Console.WriteLine(username);
-}
+    {
+        Console.Write ("Привет, ");
+        Console.WriteLine(username);
+    }
