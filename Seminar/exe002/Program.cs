a6ebb0c86bb3da366c75fe40ca7607bf7e983dd8
ZaytsevM <mivlz@mail.ru>
2022-08-09 21:32:51 +0300

    Убрать вторую цифру из трехзначного числа

---
+++ Seminar/exe002/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
+
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int sto = number/100;
+int dec = number%10;
+Console.WriteLine($"{sto}{dec}");
+
