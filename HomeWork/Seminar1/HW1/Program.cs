17c5c732ad135d1c182306e8d02598527971a1d6
ZaytsevM <mivlz@mail.ru>
2022-08-09 17:44:28 +0300

    Проверка максимального числа из 2 задаваемых пользователем чисел

--- HomeWork/Seminar1/HW1/Program.cs
+++ HomeWork/Seminar1/HW1/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.Clear();
-
 //Перечисление двух чисел из которых ищется максимальное
 Console.Write("Вставьте число a = ");
 int numberA = Convert.ToInt32(Console.ReadLine());
@@ -7,7 +6,6 @@ Console.Write("Вставьте число b = ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 
 //Логика по вычислению максимума
-
 int max = numberA;
     while(numberA > max) max = numberA;
     while(numberB > max) max = numberB;
