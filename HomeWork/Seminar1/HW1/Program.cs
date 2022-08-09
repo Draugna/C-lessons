2ca692170cee78114d1d85d6afb81b978e732b10
ZaytsevM <mivlz@mail.ru>
2022-08-09 16:48:42 +0300

    Homework

---
+++ HomeWork/Seminar1/HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+//Перечисление двух чисел из которых ищется максимальное
+Console.Write("Вставьте число a = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Вставьте число b = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+//Логика по вычислению максимума
+
+int max = numberA;
+    while(numberA > max) max = numberA;
+    while(numberB > max) max = numberB;
+
+    Console.Write("Максимальное число равно, " + max);
