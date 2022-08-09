93c0a9de7eb1684327718630cb956b157f5e1a41
ZaytsevM <mivlz@mail.ru>
2022-08-09 20:46:15 +0300

    Самое большое число в двузначном числе

---
+++ Seminar/exe001/Program.cs
@@ -0,0 +1,11 @@
+﻿int number1 = new Random().Next(10, 100);
+Console.WriteLine(number1);
+
+if (number1/10>number1%10)
+{
+    Console.WriteLine(number1/10);
+}
+else
+{
+    Console.WriteLine(number1%10);
+}
