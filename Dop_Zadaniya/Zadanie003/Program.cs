04d155af691be7c73acfba17efaed539453f6cb0
ZaytsevM <mivlz@mail.ru>
2022-08-11 02:44:32 +0300

    Задание №3

--- Dop_Zadaniya/Zadanie003/Program.cs
+++ Dop_Zadaniya/Zadanie003/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Генерируются два случайных целых числа c и d в интервале [-10 – 10].
+// Если оба числа четные, выводится их частное, если оба нечетные - их сумма,
+// если одно число четное, а другое нечетное, выводится их произведение ( if ),
+
+Console.Clear();
+
+int c = new Random().Next(-10, 11);
+Console.WriteLine($"Первое число : {c}");
+int d = new Random().Next(-10, 11);
+Console.WriteLine($"Второе число : {d}");
+int total = -1;
+
+if (c % 2 == 0 && d % 2 == 0)
+    {
+        total = c / d;
+        Console.WriteLine($"Деление : {total}");
+    }
+    else if ( c % 2 != 0 ^ d % 2 != 0)
+    {
+        total = c + d;
+        Console.Write($"Сложение : {total}");
+    }
+    else 
+    {
+        total = c * d;
+        Console.Write($"Умножение : {total}");
+    }
