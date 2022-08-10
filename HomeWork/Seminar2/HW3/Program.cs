0419357750362ad0bdbf9b6fbaa4409f5eefa990
ZaytsevM <mivlz@mail.ru>
2022-08-10 22:38:26 +0300

    Выявление выходных по заданному числу

--- HomeWork/Seminar2/HW3/Program.cs
+++ HomeWork/Seminar2/HW3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+Console.Clear();
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int week = 7;
+int Day = num;
+
+if (num > 8)
+{
+do
+{
+    num = num - week;
+}
+while (num > 8);
+}
+else
+{
+    num = num;
+}
+if( num == 6 ^ num == 7)
+        {
+            Console.WriteLine($"Заданное число {Day} - Выходной!");
+        }
+    else
+        {
+            Console.WriteLine($"Заданное число {Day} -  Будний день!");
+        }
+Console.Write($"Это {num} день недели!");
+
