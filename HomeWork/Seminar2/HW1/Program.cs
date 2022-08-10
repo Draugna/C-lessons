35104ab2e32575aca7bd2d464980940470c01d0f
ZaytsevM <mivlz@mail.ru>
2022-08-10 19:11:17 +0300

    Выписывание второй цифры из трехзначногочисла с проверкой ввода трехзначных чисел

--- HomeWork/Seminar2/HW1/Program.cs
+++ HomeWork/Seminar2/HW1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+ //Метод выбора только трехзначного числа с повторением ввода
+int num = -1;
+do
+{
+    Console.Write("Введите трехзначное число: ");
+    num = Convert.ToInt32(Console.ReadLine());
+} while (num < 99 ^ num > 1000);
+
+int two = num/10;
+int secondchar = two%10;
+
+
+Console.WriteLine(num);
+Console.WriteLine($"Вторая цифра из числа {num} является: {secondchar}");
+
+
