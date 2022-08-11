41e9824210f6166feabc83ce1288ac16568874f5
ZaytsevM <mivlz@mail.ru>
2022-08-11 04:03:20 +0300

    Задание №7

---
+++ Dop_Zadaniya/Zadanie007/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выводится название предмета по введенной первой букве: ф - физика, м -
+// математика, и - история, г - география, б - биология (оператор switch).
+Console.Clear();
+Console.Write("Введите первую букву предмета: ");
+string a = Console.ReadLine();
+
+switch(a)
+{
+    case "ф" : Console.Write("Физика"); break;
+    case "м" : Console.Write("Математика"); break;
+    case "и" : Console.Write("История"); break;
+    case "г" : Console.Write("География"); break;
+    case "б" : Console.Write("Биология"); break;
+    default : Console.Write("Такого предмета нет"); break;
+}
