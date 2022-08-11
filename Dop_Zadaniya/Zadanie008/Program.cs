dda844720921304727f599eb694db2fdd86836b6
ZaytsevM <mivlz@mail.ru>
2022-08-11 04:06:42 +0300

    Задание №8

---
+++ Dop_Zadaniya/Zadanie008/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выводится название страны и ее столицы по введенной первой букве:
+// б – Беларусь, Минск, р – Россия, Москва, л –Литва, Вильнюс (switch),
+
+Console.Clear();
+Console.Write("Введите первую букву Страны: ");
+string a = Console.ReadLine();
+
+switch(a)
+{
+    case "б" : Console.Write("Беларусь, Минск"); break;
+    case "р" : Console.Write("Россия, Москва"); break;
+    case "л" : Console.Write("Литва, Вильнюс"); break;
+    default : Console.Write("Такой страны нет!"); break;
+}
