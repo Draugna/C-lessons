3ee38f78224737b6ccf092c1dfbdf8073f1d636c
ZaytsevM <mivlz@mail.ru>
2022-08-11 03:33:21 +0300

    Задание №5

--- Dop_Zadaniya/Zadanie005/Program.cs
+++ Dop_Zadaniya/Zadanie005/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Проверяется соответствие веса и роста (ввод рост и вес; вывод одного
+// из сообщений "Норма", "Нужно похудеть", "Нужно поправиться".
+// Нормальный вес (в кг) = (рост (в см) – 100) ± 10%.
+Console.Clear();
+
+Console.Write("Введите свой рост: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите свой вес: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int norma = (a - 100);
+Console.WriteLine(norma);
+
+if ( norma + (norma * 0.1) < b & norma - (norma * 0.1) < b)
+    {
+        Console.Write("Вам нужно похудеть");
+    }
+    else if( norma + (norma * 0.1) > b & norma - (norma * 0.1) > b)
+    {
+        Console.Write("Вам нужно Поправиться");
+    }
+    else
+    { 
+        Console.Write("Норма");
+    }
