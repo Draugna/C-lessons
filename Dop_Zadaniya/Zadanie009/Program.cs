834dfb1cf3b6393721cf29ca65e3c62dbb84b213
ZaytsevM <mivlz@mail.ru>
2022-08-11 04:14:26 +0300

    Задание №9

---
+++ Dop_Zadaniya/Zadanie009/Program.cs
@@ -0,0 +1,26 @@
+﻿// По введенному номеру выводится название дня недели (1 – пн, 2 –вт,...)
+// и сообщение “рабочий день” или “выходной” (операторы switch, if).
+
+Console.Clear();
+Console.Write("Введите номер дня: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch(a)
+{
+    case 1 : Console.Write("пн"); break;
+    case 2 : Console.Write("вт"); break;
+    case 3 : Console.Write("ср"); break;
+    case 4 : Console.Write("чт"); break;
+    case 5 : Console.Write("пт"); break;
+    case 6 : Console.Write("сб"); break;
+    case 7 : Console.Write("вск"); break;
+    default : Console.Write("Такого дня недели нет!"); break;
+}
+if(a == 6 ^ a == 7)
+{
+    Console.Write(" - выходной");
+}
+else
+{
+    Console.Write(" - рабочий день");
+}
