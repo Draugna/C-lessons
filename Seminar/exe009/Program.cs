be24bb2bef1e4432b415a6c811c27062552f83fd
ZaytsevM <mivlz@mail.ru>
2022-08-12 21:56:04 +0300

    Квадраты всех чисел до задаваемого числа

--- Seminar/exe009/Program.cs
+++ Seminar/exe009/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+Console.Clear();
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= N; i++)
+{
+    Console.Write($"{i*i} ");
+}
