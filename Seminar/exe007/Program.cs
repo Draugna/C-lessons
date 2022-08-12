3f068657fd7c30cc6b902394fbfd89e55375a01e
ZaytsevM <mivlz@mail.ru>
2022-08-12 21:57:40 +0300

    Координаты точек по номеру четверти

--- Seminar/exe007/Program.cs
+++ Seminar/exe007/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в 
+// этой четверти (x и y).
+
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти: ");
+int a = int.Parse(Console.ReadLine());
+
+switch(a)
+{
+    case 1 : Console.WriteLine("x > 0 , y > 0"); break;
+    case 2 : Console.WriteLine("x < 0 , y > 0"); break;
+    case 3 : Console.WriteLine("x < 0 , y < 0"); break;
+    case 4 : Console.WriteLine("x > 0 , y < 0"); break;
+    default : Console.WriteLine("Данной четверти не существует"); break;
+}
