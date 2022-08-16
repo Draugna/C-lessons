f04553751fd36dd727a509ea680ff78df26230c1
ZaytsevM <mivlz@mail.ru>
2022-08-16 22:02:37 +0300

    Произведение чисел от 1 до A

--- Seminar/exe012/Program.cs
+++ Seminar/exe012/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход число (А)
+// и выдаёт произведение чисел от 1 до А.
+
+int GetSumNums(int number)
+{
+    int sum = 1;
+    for (int i = 1; i <= Math.Abs(number); i++)
+    {
+        sum *= i;
+    } 
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Cумма цифр от 1 до введенного числа: {GetSumNums(num)}");
