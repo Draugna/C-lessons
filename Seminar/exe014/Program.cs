3cc17643747bc47a63984daed8e41f35280fa274
ZaytsevM <mivlz@mail.ru>
2022-08-17 22:42:20 +0300

    Создание 8 числового масива с значениями от 0 до 1

--- Seminar/exe014/Program.cs
+++ Seminar/exe014/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.
+Console.Clear();
+
+void NewArray(int[] ar)
+{
+    int length = ar.Length;
+    Console.Write("[");
+    for(int index = 0; index < length; index++)
+    {
+        ar[index] = new Random().Next(0,2);
+        if (index >= length - 1)
+        {
+            Console.Write($"{ar[index]}");
+        }
+        else
+        {
+        Console.Write($"{ar[index]},");
+        }
+    }
+    Console.Write("]"); 
+}
+int[] array = new int[8];
+NewArray(array);
+
