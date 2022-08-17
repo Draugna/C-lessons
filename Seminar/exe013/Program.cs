b8e1fcfb850b3653e08c00d47d32df6fd7b5a476
ZaytsevM <mivlz@mail.ru>
2022-08-17 22:04:48 +0300

    Произведение чисел от 1 до N

--- Seminar/exe013/Program.cs
+++ Seminar/exe013/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число N и выдаёт 
+// произведение чисел от 1 до N.
+
+Console.Clear();
+
+void Multiply(int a)
+{
+    int mltp = 1;
+    for(int i = 1; i <= a; i++)
+    {
+        mltp *= i;
+    }
+    
+    
+    Console.Write($"Произведение чисел от 1 до {a} равна: {mltp}");
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Multiply(N);
