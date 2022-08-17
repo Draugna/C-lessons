8b49f34b9bd08abcc2192eec047ac1695eff6641
ZaytsevM <mivlz@mail.ru>
2022-08-17 21:45:18 +0300

    Возведение числа в степень

--- HomeWork/Seminar4/HW1/Program.cs
+++ HomeWork/Seminar4/HW1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+void Multiply (int z, int x)
+{
+    int sqr = 1;
+    for (int i = 0; i<x; i++)
+    {
+        sqr *= z;
+    }
+    Console.WriteLine($"Число {z} в степени {x} равно: {sqr}");
+}
+
+
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+
+Multiply(A,B);
