7431a6ca64e355a3a685d4397b71719890f78058
ZaytsevM <mivlz@mail.ru>
2022-08-17 21:56:30 +0300

    Сумма чисел введенного числа

--- HomeWork/Seminar4/HW2/Program.cs
+++ HomeWork/Seminar4/HW2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход число и выдаёт 
+//  сумму цифр в числе.
+
+Console.Clear();
+
+void SumOfNumber (int a)
+{
+    int sum = 0;
+    int b = a;
+    for (sum = 0; b > 0; b /= 10)
+    {
+        sum = sum + b%10;
+    }
+
+    Console.Write($"Сумма чисел числа {a} равна: {sum}");
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+SumOfNumber(num);
