6f73984c65ca5c105a762a967bf6faf7b11ab8b1
ZaytsevM <mivlz@mail.ru>
2022-08-23 12:03:25 +0300

    Подсчет рандомных чисел в масиве из 8 чисел на кол-во четных

--- HomeWork/Seminar5/HW1/Program.cs
+++ HomeWork/Seminar5/HW1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными 
+// числами. Напишите программу, которая покажет количество чётных чисел 
+// в массиве.
+
+Console.Clear();
+
+void Massive(int[] ar)
+{
+    int length = ar.Length;
+    for(int i = 0; i < length; i++)
+    {
+        ar[i] = new Random().Next(100 , 1000);
+        Console.Write($"{ar[i]} ");
+    }
+}
+int[] array = new int[8];
+Massive(array);
+
+int a = 0;
+int index = 0;
+do
+{
+    if (array[index] % 2 != 0)
+    {
+        a++;
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+}while ( index < array.Length);
+
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в масиве: {a}");
