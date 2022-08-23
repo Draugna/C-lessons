628a38edb022a14e0184b4c62a0d54da3208858b
ZaytsevM <mivlz@mail.ru>
2022-08-23 13:03:00 +0300

    Разность между максимальным и минимальным элементом массива

--- HomeWork/Seminar5/HW3/Program.cs
+++ HomeWork/Seminar5/HW3/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным 
+// и минимальным элементов массива.
+
+Console.Clear();
+
+void Massive(int[] ar)
+{
+    int length = ar.Length;
+    for(int i = 0; i < length; i++)
+    {
+        ar[i] = new Random().Next(-1000 , 1000);
+        Console.Write($"{ar[i]} ");
+    }
+}
+int[] array = new int[8];
+Massive(array);
+
+int max = array[0];
+int min = array[0];
+int index = 0;
+do{
+if(array[index] > max)
+    {
+        max = array[index];
+    }
+else if(array[index] < min) 
+{
+    min = array[index];
+}
+else
+{
+
+}
+index++;
+}while(index < array.Length);
+
+Console.WriteLine();
+
+Console.WriteLine($" Минимальное число: {min}");
+Console.WriteLine($" Максимальное число: {max}");
+int itog = max - min;
+Console.WriteLine($" Разность максимального и минимального числа равна: {itog}");
