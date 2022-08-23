ec2a5ed2c3512989eb8b883c49753f4aa16023e0
ZaytsevM <mivlz@mail.ru>
2022-08-23 21:05:31 +0300

    Сумма положительных чисел массива

--- Seminar/exe016/Program.cs
+++ Seminar/exe016/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вычислить среднее арифметическое положительных элементов в 
+// одномерном массиве. 
+
+Console.Clear();
+
+void SumElem(int[] arr)
+{
+    double sum = 0;
+    int n = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]>0)
+        {
+            sum +=  arr[i];
+            n++;
+        }
+    }
+    Console.Write($"{sum/n:f2}");
+}
+
+
+int[] array = {1, -5, 8, 4, -9};
+SumElem(array);
