f67ecd0be93e4c4485807a8525542d90cddde64e
ZaytsevM <mivlz@mail.ru>
2022-08-23 21:05:02 +0300

    Разворот масива

--- Seminar/exe015/Program.cs
+++ Seminar/exe015/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на 
+// последнем и т.д.). 
+
+void ReverseArray(int[] arr)
+{
+    int count = arr.Length;
+    int[] result = new int[count];
+
+    for(int i = 0; i < count; i++)
+    {
+        result[i] = arr[count-1-i];
+        Console.Write(result[i]);
+    }
+
+}
+
+int[] array = {1,2,3,4,5};
+ReverseArray(array);
