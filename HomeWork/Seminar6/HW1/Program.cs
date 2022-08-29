2c1b47d601a74bc8adfe8e2ce0cdd0f8ac8c27dd
ZaytsevM <mivlz@mail.ru>
2022-08-29 21:03:00 +0300

    Сколько чисел больше нуля ввел пользователь

---
+++ HomeWork/Seminar6/HW1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Колличество элементов больше нуля: {count}");
