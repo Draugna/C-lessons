5a8944de0fcd9f048152c91443687676dd1e540d
ZaytsevM <mivlz@mail.ru>
2022-09-19 21:05:00 +0300

    Вывод трехмерного массива

--- HomeWork/Seminar8/HW4/Program.cs
+++ HomeWork/Seminar8/HW4/Program.cs
@@ -1,3 +1,64 @@
 ﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, 
 // добавляя индексы каждого элемента.
+
+Console.Clear();
+
+Console.Write("Введите размеры масива через пробел: ");
+string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]) }, 10, 99);
+PrintArray(array);
+
+
+int[,,] GetArray(int[] sizes, int min, int max)
+{
+    int[,,] result = new int[sizes[0],sizes[1],sizes[2]];
+    for(int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j <result.GetLength(1); j++)
+        {
+            int k = 0;
+            while(k<result.GetLength(2))
+            {
+                int element=new Random().Next(min,max + 1);
+                if(FindElement(result,element)) continue;
+                result[i,j,k] = element;
+                k++;
+            }
+        }
+    }
+    return result;
+}
+
+bool FindElement(int[,,] array, int el)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                if(array[i,j,k]==el) return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+
+void PrintArray(int[,,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2);k++)
+            {
+            Console.Write($"{array[i,j,k]} ({i},{j},{k})  ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
